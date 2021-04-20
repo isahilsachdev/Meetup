33bbecc8eec9eef6246e890abf3f6d00267d2a52
isahilsachdev <isahilsachdev00@gmail.com>
2021-04-20 21:32:03 +0530

    meetup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import { Routes } from './Routes/Routes';
+import { Footer } from './Components/Footer';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Routes />
+      <Footer />
+      {/* <Profile /> */}
+
+    </div>
+  );
+}
+
+export default App;
